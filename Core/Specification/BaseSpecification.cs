13bd2ce0fba25b62a82e54fc43c176f742650a69
ifratmitul <ifrat.mitul@hotmail.com>
2020-12-22 04:12:41 +0600

    adding filtering option based on name, asc & desc order

--- Core/Specification/BaseSpecification.cs
+++ Core/Specification/BaseSpecification.cs
@@ -26,9 +26,28 @@ namespace Core.Specification
         public List<Expression<Func<T, object>>> Includes { get; } =
          new List<Expression<Func<T, object>>>();
 
+
+
         protected void AddInclude(Expression<Func<T, object>> includeExpression)
         {
             Includes.Add(includeExpression);
+        }
+
+
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
         }
     }
 }
