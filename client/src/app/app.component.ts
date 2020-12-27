e0f669efb322ea8887ce9e828c1f88b6e45d397d
ifratmitul <ifrat.mitul@hotmail.com>
2020-12-27 18:45:03 +0600

    made api call, & added interfaces for Product and Pagination

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/Pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  product: IProduct[];
+
+  constructor(private http: HttpClient ) {  }
+
+  ngOnInit(){
+
+    this.http.get('https://localhost:5001/api/products').subscribe((response : IPagination) => 
+    {
+        console.log(response);
+        this.product = response.data;
+    }, 
+    error => 
+    {
+        console.log(error);
+    })
+  }
 }
